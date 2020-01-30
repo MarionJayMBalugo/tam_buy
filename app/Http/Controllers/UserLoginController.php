cbd66721c220c948aa3bf46296abf49008f9fcef
2ndyrGroupB <2ndyrGroupB@pn.local>
2020-01-30 14:45:49 +0800

    initializedLoginAndRegister

--- app/Http/Controllers/UserLoginController.php
+++ app/Http/Controllers/UserLoginController.php
@@ -31,10 +31,6 @@ class UserLoginController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    // public function home()
-    // {
-    //     return view('landingPage');
-    // }
 
     public function showLogin()
     {
